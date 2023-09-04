e543899b39e968a0376280c6d24d59d285b0ab59
murlikrp <31443425+murlikrp@users.noreply.github.com>
2023-09-04 16:41:03 -0700

    updated

---
+++ src/main/java/com/example/ControlService/ControlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ControlService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlServiceApplication.class, args);
+	}
+
+}
