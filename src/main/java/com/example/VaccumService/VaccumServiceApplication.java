8bc1bea267a457b9d0457172e7b2bcd73d033063
murlikrp <31443425+murlikrp@users.noreply.github.com>
2023-09-04 16:22:42 -0700

    Added springboot project

---
+++ src/main/java/com/example/VaccumService/VaccumServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.VaccumService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccumServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccumServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VaccumService/VaccumServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VaccumService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccumServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
